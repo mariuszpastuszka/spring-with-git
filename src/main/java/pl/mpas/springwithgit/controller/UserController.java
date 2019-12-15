f27efdfb0ebed0e328f3cc3b1292b43659e31c15
mpastuszka001 <m@op.pl>
2019-12-15 10:33:35 +0100

    [add] - first endpoint method

--- src/main/java/pl/mpas/springwithgit/controller/UserController.java
+++ src/main/java/pl/mpas/springwithgit/controller/UserController.java
@@ -1,4 +1,13 @@
 package pl.mpas.springwithgit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
 }
