6d961cb2204e18914e7417e2b476261e456b9d34
maniek <m@op.pl>
2019-12-15 11:24:29 +0100

    Revert "[change] - new address"
    This reverts commit ec737627

--- src/main/java/pl/mpas/springwithgit/controller/UserController.java
+++ src/main/java/pl/mpas/springwithgit/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-    @GetMapping("/bad-home")
+    @GetMapping("/home")
     public String home() {
         return "home";
     }
